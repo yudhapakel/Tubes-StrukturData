1a5d23798282d1fa9b2e4a46ea06243fed1c7ef8
YudhaHarwanto <yudahp23@gmail.com>
2024-12-06 02:37:09 +0700

    minor changes: struct ruteWisata added

--- ruteWisata.h
+++ ruteWisata.h
@@ -1,7 +1,22 @@
-#ifdef RUTEWISATA_H_INCLUDED
+#ifndef RUTEWISATA_H_INCLUDED
 #define RUTEWISATA_H_INCLUDED
 
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+typedef string NamaWisata;
+
+struct ruteWisata{
+    NamaWisata nama;
+    string lokasi;
+    double jarak;
+};
+
+
+
+
 
 
 
