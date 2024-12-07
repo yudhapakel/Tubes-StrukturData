217f7645143527cf0f3c892947345c1944f02f39
YudhaHarwanto <yudahp23@gmail.com>
2024-12-07 20:47:56 +0700

    major changes: add function on ruteWisata.h and ruteWisata.cpp

--- ruteWisata.h
+++ ruteWisata.h
@@ -3,11 +3,17 @@
 
 #include <iostream>
 #include <string>
+#include <climits>
+#include <cmath>
 
 using namespace std;
 
+const int MAX_TEMPAT = 100;
+
+typedef namaWisata;
+
 struct ruteWisata{
-   string nama;
+   namaWisata nama;
     string lokasi;
     double jarak;
 };
@@ -19,6 +25,8 @@ bool validasiInput(double jarak);
 void cariTempatWisata(ruteWisata rute[], int n, string nama);
 double estimasiWakti(double jarak, double kecepatan);
 void rekomendasiTempatWisata(ruteWisata rute[], int n);
+void hitungJarakTerpendek(double graph[MAX_TEMPAT], int n, int start, int end);
+void salesmanProblem(double graph[MAX_TEMPAT], int n);
 
 
 #endif // RUTEWISATA_H_INCLUDED
